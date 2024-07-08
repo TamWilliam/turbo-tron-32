1740583b02a4475fbc3546b6c657bc821921a0d5
remi <remi.petit93370@gmail.com>
2024-07-08 13:28:48 +0200

    fichiers de conf

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,11 +3,12 @@ import { View, Text } from 'react-native';
 import { ReactNativeJoystick } from '@korsolutions/react-native-joystick';
 
 const App = () => {
-  const [joystickCoords, setJoystickCoords] = useState({ x: 0, y: 0 });
-  const [joystickCoordsRepere, setJoystickCoordsRepere] = useState({ x2: 0, y2: 0 });
+  const [joystickCoordsRepere, setJoystickCoordsRepere] = useState({ x: 0, y: 0 });
+  const [force, setForce] = useState(0);
   const [dataAngle, setDataAngle] = useState(0);
   const [radian, setRadian] = useState(0);
   const [ws, setWs] = useState(null);
+  const [wsInformations, setWsInformations] = useState("");
 
   useEffect(() => {
     const websocket = new WebSocket('wss://192.168.122.50/ws'); // Remplacez par l'adresse IP de votre ESP32
@@ -15,18 +16,22 @@ const App = () => {
     websocket.onopen = () => {
       console.log('Connected to WebSocket server');
       setWs(websocket);
+      setWsInformations("Connected to WebSocket server")
     };
 
     websocket.onmessage = (event) => {
       console.log('Received:', event.data);
+      setWsInformations('Received:')
     };
 
     websocket.onclose = () => {
       console.log('Disconnected from WebSocket server');
+      setWsInformations('Disconnected from WebSocket server')
     };
 
     websocket.onerror = (error) => {
       console.error('WebSocket error:', error);
+      setWsInformations('WebSocket error')
     };
 
     return () => {
@@ -41,36 +46,28 @@ const App = () => {
   };
 
   const handleJoystickMove = (data) => {
-    let { x, y } = data.position;
     let { screenX, screenY } = data.position;
     const { degree, radian } = data.angle;
+    let force = data.force;
 
-    // Calcul de la distance par rapport à l'origine
-    const distance = Math.sqrt(screenX * screenX + screenY * screenY);
-    const maxDistance = 1.5;
-    
-    if (distance > maxDistance) {
-        // Limiter les coordonnées à la distance maximale en fonction de l'angle
-        screenX = maxDistance * Math.cos(radian);
-        screenY = maxDistance * Math.sin(radian);
-    }
-
+    // Mettre la valeur en pourcentage
     screenX = screenX / 3 * 2 * 100;
     screenY = screenY / 3 * 2 * 100;
+    force = force / 3 * 2 * 100;
 
-    setJoystickCoords({ x, y });
-    setJoystickCoordsRepere({ x2: screenX, y2: screenY });
+    setJoystickCoordsRepere({ x: screenX, y: screenY });
+    setForce(force);
     setDataAngle(degree);
     setRadian(radian);
   };
 
   useEffect(() => {
-    if (joystickCoordsRepere.y2 > 0) {
+    if (joystickCoordsRepere.y > 0) {
       sendCommand({
         cmd: 1,
         data: [1000, 1000, 1000, 1000]
       });
-    } else if (joystickCoordsRepere.y2 < 0) {
+    } else if (joystickCoordsRepere.y < 0) {
       sendCommand({
         cmd: 1,
         data: [-1000, -1000, -1000, -1000]
@@ -81,15 +78,16 @@ const App = () => {
         data: [0, 0, 0, 0]
       });
     }
-  }, [joystickCoordsRepere.y2]);
+  }, [joystickCoordsRepere.y]);
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#038ac9' }}>
       <ReactNativeJoystick onMove={handleJoystickMove} onStop={handleJoystickMove} backgroundColor="#d9d9d9" color="#959292" radius={75}/>
-      <Text>Coordonnées du joystick : ({joystickCoords.x.toFixed(2)}, {joystickCoords.y.toFixed(2)})</Text>
-      <Text>Coordonnées : ({joystickCoordsRepere.x2.toFixed(2)}, {joystickCoordsRepere.y2.toFixed(2)})</Text>
+      <Text>Coordonnées du joystick : ({joystickCoordsRepere.x.toFixed(2)}, {joystickCoordsRepere.y.toFixed(2)})</Text>
+      <Text>Force : {force}%</Text>
       <Text>Angle : ({dataAngle})</Text>
       <Text>Radian : ({radian})</Text>
+      <Text>Websocket info : {wsInformations}</Text>
     </View>
   );
 };
