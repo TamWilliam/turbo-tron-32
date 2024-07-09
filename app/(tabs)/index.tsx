05f3e665a9c9c660ae1de14c34f11774c3146e37
Douv-code <david.pikusa@gmail.com>
2024-07-09 13:48:09 +0200

    mouvement de la voiture avec joystick

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,95 +1,70 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text } from 'react-native';
-import { ReactNativeJoystick } from '@korsolutions/react-native-joystick';
+import { Image, StyleSheet, Platform } from 'react-native';
 
-const App = () => {
-  const [joystickCoordsRepere, setJoystickCoordsRepere] = useState({ x: 0, y: 0 });
-  const [force, setForce] = useState(0);
-  const [dataAngle, setDataAngle] = useState(0);
-  const [radian, setRadian] = useState(0);
-  const [ws, setWs] = useState(null);
-  const [wsInformations, setWsInformations] = useState("");
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 
-  useEffect(() => {
-    const websocket = new WebSocket('wss://192.168.122.50/ws'); // Remplacez par l'adresse IP de votre ESP32
-
-    websocket.onopen = () => {
-      console.log('Connected to WebSocket server');
-      setWs(websocket);
-      setWsInformations("Connected to WebSocket server")
-    };
-
-    websocket.onmessage = (event) => {
-      console.log('Received:', event.data);
-      setWsInformations('Received:')
-    };
-
-    websocket.onclose = () => {
-      console.log('Disconnected from WebSocket server');
-      setWsInformations('Disconnected from WebSocket server')
-    };
-
-    websocket.onerror = (error) => {
-      console.error('WebSocket error:', error);
-      setWsInformations('WebSocket error')
-    };
-
-    return () => {
-      websocket.close();
-    };
-  }, []);
-
-  const sendCommand = (command) => {
-    if (ws && ws.readyState === WebSocket.OPEN) {
-      ws.send(JSON.stringify(command));
-    }
-  };
-
-  const handleJoystickMove = (data) => {
-    let { screenX, screenY } = data.position;
-    const { degree, radian } = data.angle;
-    let force = data.force;
-
-    // Mettre la valeur en pourcentage
-    screenX = screenX / 3 * 2 * 100;
-    screenY = screenY / 3 * 2 * 100;
-    force = force / 3 * 2 * 100;
-
-    setJoystickCoordsRepere({ x: screenX, y: screenY });
-    setForce(force);
-    setDataAngle(degree);
-    setRadian(radian);
-  };
-
-  useEffect(() => {
-    if (joystickCoordsRepere.y > 0) {
-      sendCommand({
-        cmd: 1,
-        data: [1000, 1000, 1000, 1000]
-      });
-    } else if (joystickCoordsRepere.y < 0) {
-      sendCommand({
-        cmd: 1,
-        data: [-1000, -1000, -1000, -1000]
-      });
-    } else {
-      sendCommand({
-        cmd: 1,
-        data: [0, 0, 0, 0]
-      });
-    }
-  }, [joystickCoordsRepere.y]);
-
+export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: '#038ac9' }}>
-      <ReactNativeJoystick onMove={handleJoystickMove} onStop={handleJoystickMove} backgroundColor="#d9d9d9" color="#959292" radius={75}/>
-      <Text>Coordonnées du joystick : ({joystickCoordsRepere.x.toFixed(2)}, {joystickCoordsRepere.y.toFixed(2)})</Text>
-      <Text>Force : {force}%</Text>
-      <Text>Angle : ({dataAngle})</Text>
-      <Text>Radian : ({radian})</Text>
-      <Text>Websocket info : {wsInformations}</Text>
-    </View>
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+      headerImage={
+        <Image
+          source={require('@/assets/images/partial-react-logo.png')}
+          style={styles.reactLogo}
+        />
+      }>
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome!</ThemedText>
+        <HelloWave />
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText>
+          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
+          Press{' '}
+          <ThemedText type="defaultSemiBold">
+            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
+          </ThemedText>{' '}
+          to open developer tools.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+        <ThemedText>
+          Tap the Explore tab to learn more about what's included in this starter app.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+        <ThemedText>
+          When you're ready, run{' '}
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
+          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
   );
-};
+}
 
-export default App;
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
