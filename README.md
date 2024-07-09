1e65f05e652d6114c754a8a6aa0b76f44312b40e
remi <remi.petit93370@gmail.com>
2024-07-09 10:54:37 +0200

    code modifié du prof pour la voiture. la récupération de l'image fonctionne.

--- README.md
+++ README.md
@@ -1,9 +1,122 @@
-# TurboTron32
+# La voiture des enfers
 
-## Introduction
+Dans les grandes lignes, la voiture va chercher à se connecter à un réseau WiFi existant.
 
-Ce projet est réalisé dans le cadre d’un projet de fin d'année au sein de l’école HETIC. Il s’agit de la réalisation d’une course de voiture électrique où le but est de faire le meilleur temps par rapport aux autres groupes d’étudiants. Notre projet, Turbo Tron 32, est une application innovante de course de voitures. Notre équipe est composée de Valérie Song, Maxime Ait Adda, Rémi Petit, William Tam, David Pikusa, Jeanne Bissinga et Pierre Leroy.
+Une fois connectée, elle va prendre ses ordres en WebSocket et renvoyer sa télémétrie en MQTT.
 
-Le projet HETIC Web 3 Tech Race consiste en deux challenges : un où notre équipe contrôle le véhicule via une application que nous avons développée, et un autre où la voiture doit se repérer seule sur un parcours prédéfini. Notre application permettra aux utilisateurs de contrôler la voiture, de visualiser les statistiques en temps réel, de suivre les données de course et de visualiser la course depuis la caméra embarquée.
+Tout le code est entièrement OpenSource, il est donc possible (et même conseillé) d'aller le modifier pour pouvoir l'adapter à vos besoins.
 
-Notre projet se divise en deux parties : le niveau 1, où nous développons une application mobile pour contrôler le véhicule et afficher les statistiques en temps réel, et le niveau 2, où nous permettons à la voiture de fonctionner en totale autonomie et de remporter la course.
+## Quick start
+
+Ce projet est fait pour fonctionner avec [PlatformIO](https://platformio.org/). C'est un plugin VS Code qui va vour permettre de remplacer le traditionnel Arduino IDE au profit d'un framework embarquant une structure plus classique de projet C++ avec un fichier de configuration qui emporte toutes les informations et dépendances de votre projet.
+
+Pour uploader le code sur votre voiture, il vous suffit de brancher votre ESP32 en USB et de cliquer sur le bouton "upload".
+
+## Adapter quelques éléments
+
+Il faudra au moins adapter quelques éléments du code pour rentrer les crédentials de votre WiFi, l'ip que vous allez donner à la voiture, la gateway et le netmask de votre réseau.
+Dans `src/main.cpp` vous allez modifier les lignes suivantes:
+
+```cpp
+// ...
+
+char *ssid_wifi = "OnePlus 7 Pro";    // Le nom du réseau WiFi
+char *password_wifi = "33500a9695df"; // Le password du WiFi
+
+const char *mqtt_server = "192.168.145.40"; // L'IP de votre broker MQTT
+const int mqtt_interval_ms = 5000;          // L'interval en ms entre deux envois de données
+
+IPAddress localIP(192, 168, 145, 49); // l'IP que vous voulez donner à votre voiture
+
+IPAddress localGateway(192, 168, 145, 55); // L'IP de la gateway de votre réseau
+IPAddress localSubnet(255, 255, 255, 0);   // Le masque de sous réseau
+
+// ...
+AsyncWebSocket ws("/ws"); // Changez le nom de ce point d'accès pour "sécuriser" l'accès à votre voiture
+```
+
+## Communiquer avec la voiture
+
+Les ordre envoyés à la voiture se font via WebSocket à l'addresse suivante : `ws://<CAR_IP_ADDRESS>/ws`
+
+L'intégralité des ordres seront des JSON avec deux clés:
+
+-   cmd: l'id du type d'ordre que vous voulez passer
+-   data: la donnée associée à la commande
+
+Par exemple, si vous voulez changer la vitesse des roues
+
+```json
+{
+	"cmd": 1,
+	"data": [2000, 2000, 2000, 2000]
+}
+```
+
+Ici, data est un array où chaque valeur correspond à la vitesse d'une roue
+
+On pourrait aussi avoir l'exemple suivant pour assigner le "visage" 3 sur la voiture:
+
+```json
+{
+	"cmd": 2,
+	"data": 3
+}
+```
+
+## Liste des commandes
+
+| CMD NAME                   | CMD ID | DATA TYPE | MISC                                                                                                                                                                                                                                                                      |
+| -------------------------- | ------ | --------- | ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- |
+| Roues                      | 1      | Array[4]  | Les valeurs doivent être comprises entre -4095 et 4095                                                                                                                                                                                                                    |
+| Visage                     | 2      | int       | Chaque valeur correspond à un visage différent, passé 7 les visages sont statiques et aléatoires                                                                                                                                                                          |
+| Angle de la tête           | 3      | Array[2]  | Valeurs comprises entre 0 et 180, la première correspond à l'angle vertical, la seconde à l'angle horizontal                                                                                                                                                              |
+| Animation des LED          | 4      | int       | Chaque valeur correspond à une animation. Les valeurs sont entre 0 et 5                                                                                                                                                                                                   |
+| Couleur principale des LED | 5      | Array[4]  | La première valeur est la représentation binaire d'un bitmask identifiant la LED, les 3 valeurs suivantes sont des valeurs RGB entre 0 et 255                                                                                                                             |
+| Couleur secondaire des LED | 6      | Array[4]  | La première valeur est la représentation binaire d'un bitmask identifiant la LED, les 3 valeurs suivantes sont des valeurs RGB entre 0 et 255. Si la première valeur est 0, le changement affectera toutes les LED. Ce réglage n'est visible que sur certaines animations |
+| Buzzer Alarm               | 7      | int       | Off: 0, On: 1. Déclenche le buzzer d'alarme                                                                                                                                                                                                                               |
+| Buzzer Variable            | 8      | Array[2]  | La première valeur est un ON/OFF (1/0), la seconde est la fréquence du son (entre 0 et 10000Hz)                                                                                                                                                                           |
+| Video Activation           | 9      | int       | Off: 0, On: 1. Active ou désactive le flux video                                                                                                                                                                                                                          |
+
+### Exemples
+
+#### Allumer une LED en rouge
+
+On doit d'abbord lancer une animation de LED, 1 est un allumage statique continu
+
+```json
+{
+	"cmd": 4,
+	"data": 1
+}
+```
+
+Je vais ensuite allumer les LED 1, 2, 3 et 4 en rouge
+
+```json
+{
+	"cmd": 5,
+	"data": [1, 255, 0, 0]
+}
+```
+
+```json
+{
+	"cmd": 5,
+	"data": [2, 255, 0, 0]
+}
+```
+
+```json
+{
+	"cmd": 5,
+	"data": [4, 255, 0, 0]
+}
+```
+
+```json
+{
+	"cmd": 5,
+	"data": [8, 255, 0, 0]
+}
+```
---
+++ src/main.cpp
@@ -0,0 +1,422 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiAP.h>
+#include "esp_camera.h"
+#include "Freenove_4WD_Car_WiFi.h"
+#include "Freenove_4WD_Car_Emotion.h"
+#include "Freenove_4WD_Car_WS2812.h"
+#include "Freenove_4WD_Car_For_ESP32.h"
+#include <PubSubClient.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <ArduinoJson.h>
+#include <limits.h>
+
+#define STREAM_CONTENT_BOUNDARY "123456789000000000000987654321"
+
+// Ressources
+// https://randomnerdtutorials.com/esp32-websocket-server-arduino/#1
+// https://randomnerdtutorials.com/esp32-mqtt-publish-subscribe-arduino-ide/
+// https://randomnerdtutorials.com/esp32-static-fixed-ip-address-arduino-ide/
+// https://github.com/Freenove/Freenove_4WD_Car_Kit_for_ESP32/tree/master
+
+char *ssid_wifi = "Denji 3.0";            // Le nom du réseau WiFi
+char *password_wifi = "dawarchat"; // Le password du WiFi
+
+const char *mqtt_server = "192.168.122.98"; // L'IP de votre broker MQTT
+const int mqtt_interval_ms = 5000;          // L'interval en ms entre deux envois de données
+
+IPAddress localIP(192, 168, 122, 50); // l'IP que vous voulez donner à votre voiture
+
+IPAddress localGateway(192, 168, 122, 99); // L'IP de la gateway de votre réseau
+IPAddress localSubnet(255, 255, 255, 0);  // Le masque de sous réseau
+
+IPAddress primaryDNS(8, 8, 8, 8);
+IPAddress secondaryDNS(8, 8, 4, 4);
+
+AsyncWebServer server(80);
+AsyncWebSocket ws("/ws"); // Changez le nom de ce point d'accès pour "sécuriser" l'accès à votre voiture
+
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+WiFiServer server_Cmd(4000);
+WiFiServer server_Camera(7000);
+bool videoFlag = 0;
+
+long last_message = 0;
+
+int distance[4];          // Storage of ultrasonic data
+int sensor_v;             // Int cast of track sensor data
+char buff[6];             // Buffer to store the battery voltage data
+char ultrasonic_buff[10]; // Buffer to store the Ultrasonic data
+
+// put function declarations here:
+void WiFi_Init();
+void loopTask_Camera(void *pvParameters);
+void notifyClients();
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len);
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len);
+void initWebSocket();
+void reconnect();
+
+void WiFi_Init()
+{
+    ssid_Router = ssid_wifi;         // Modify according to your router name
+    password_Router = password_wifi; // Modify according to your router password
+    ssid_AP = ssid_wifi;            // ESP32 turns on an AP and calls it Sunshine
+    password_AP = password_wifi;        // Set your AP password for ESP32 to Sunshine
+    frame_size = FRAMESIZE_CIF;      // 400*296
+}
+
+void setup()
+{
+    // delay(5000);
+
+    Serial.begin(115200);
+    Serial.setDebugOutput(true);
+
+    if (!WiFi.config(localIP, localGateway, localSubnet, primaryDNS, secondaryDNS))
+    {
+        Serial.println("STA Failed to configure");
+    }
+
+    Buzzer_Setup(); // Buzzer initialization
+    WiFi_Init();    // WiFi paramters initialization
+    WiFi_Setup(0);  // Start AP Mode. If you want to connect to a router, change 1 to 0.
+    server_Cmd.begin(4000);    // Start the command server
+    server_Camera.begin(7000); // Turn on the camera server
+
+    cameraSetup(); // Camera initialization
+    camera_vflip(true);
+    camera_hmirror(true);
+    Emotion_Setup();    // Emotion initialization
+    WS2812_Setup();     // WS2812 initialization
+    PCA9685_Setup();    // PCA9685 initialization
+    Light_Setup();      // Light initialization
+    Track_Setup();      // Track initialization
+    Ultrasonic_Setup(); // Initialize the ultrasonic module
+
+    // Cette section serait peut être à virer...
+    disableCore0WDT(); // Turn off the watchdog function in kernel 0
+    xTaskCreateUniversal(loopTask_Camera, "loopTask_Camera", 8192, NULL, 0, NULL, 0);
+    xTaskCreateUniversal(loopTask_WTD, "loopTask_WTD", 8192, NULL, 0, NULL, 0);
+
+    client.setServer(mqtt_server, 1883);
+
+    initWebSocket();
+
+    server.on("/", HTTP_GET, [](AsyncWebServerRequest *request) { 
+        camera_fb_t *fb = NULL;
+        fb = esp_camera_fb_get();
+                if (fb != NULL)
+                {
+                    uint8_t slen[4];
+                    slen[0] = fb->len >> 0;
+                    slen[1] = fb->len >> 8;
+                    slen[2] = fb->len >> 16;
+                    slen[3] = fb->len >> 24;
+                    AsyncResponseStream *response = request->beginResponseStream("image");
+                    // response->write(slen, 4);
+                    response->write(fb->buf, fb->len);
+                    request->send(response);
+                    // client.write(slen, 4);
+                    // client.write(fb->buf, fb->len);
+                    // request->send_P(200, "application/octet-stream", fb->buf, fb->len);
+                    // request->send(fb->buf, "application/octet-stream", fb->len);
+                    // Serial.println("Camera send");
+                    esp_camera_fb_return(fb);
+                    fb = NULL;
+                } });
+
+    server.begin();
+
+    // Init the state of the car
+    Emotion_SetMode(1);
+    WS2812_SetMode(1);
+}
+
+void loop()
+{
+    // put your main code here, to run repeatedly:
+    ws.cleanupClients();
+
+    Emotion_Show(emotion_task_mode); // Led matrix display function
+    WS2812_Show(ws2812_task_mode);   // Car color lights display function
+
+    // The MQTT part
+    if (!client.connected())
+    {
+        reconnect();
+    }
+    client.loop();
+
+    long now = millis();
+    if (now - last_message > mqtt_interval_ms)
+    {
+        last_message = now;
+
+        // Les led et la batteries sont branchés tous les deux sur le pin 32
+        // du coup, lire la valeur de batterie fait freeze la batterie
+        // Battery level
+        // dtostrf(Get_Battery_Voltage(), 5, 2, buff);
+        // client.publish("esp32/battery", buff);
+
+        // Track Read
+        Track_Read();
+        sensor_v = static_cast<int>(sensorValue[3]);
+        char const *n_char = std::to_string(sensor_v).c_str();
+        client.publish("esp32/track", n_char);
+
+        // Ultrasonic Data
+        dtostrf(Get_Sonar(), 5, 2, ultrasonic_buff);
+        client.publish("esp32/sonar", ultrasonic_buff);
+
+        // Photosensitive Data
+        dtostrf(Get_Photosensitive(), 5, 2, ultrasonic_buff);
+        client.publish("esp32/light", ultrasonic_buff);
+    }
+}
+
+// put function definitions here:
+void notifyClients()
+{
+    ws.textAll("ok");
+}
+
+void handleWebSocketMessage(void *arg, uint8_t *data, size_t len)
+{
+    AwsFrameInfo *info = (AwsFrameInfo *)arg;
+
+    // Serial.println((char *)data);
+
+    if (info->final && info->index == 0 && info->len == len && info->opcode == WS_TEXT)
+    {
+        data[len] = 0;
+
+        StaticJsonDocument<200> doc;
+
+        DeserializationError error = deserializeJson(doc, (char *)data);
+
+        if (error)
+        {
+            Serial.print("deserializeJson() failed: ");
+            Serial.println(error.c_str());
+            return;
+        }
+
+        int cmd = doc["cmd"];
+
+        if (1 == cmd)
+        {
+            JsonArray data = doc["data"];
+            int data_0 = data[0];
+            int data_1 = data[1];
+            int data_2 = data[2];
+            int data_3 = data[3];
+
+            Motor_Move(data_0, data_1, data_2, data_3);
+        }
+        else if (2 == cmd)
+        {
+            int data = doc["data"];
+            Emotion_SetMode(data);
+        }
+        else if (3 == cmd)
+        {
+            JsonArray angles = doc["data"];
+            int angle_0 = angles[0];
+            int angle_1 = angles[1];
+            Servo_1_Angle(angle_0); // Set the Angle value of servo 1 to 0 to 180°
+            Servo_2_Angle(angle_1);
+        }
+        else if (4 == cmd)
+        {
+            int led_mode = doc["data"];
+            WS2812_SetMode(led_mode);
+        }
+        else if (5 == cmd)
+        {
+            JsonArray led_color = doc["data"];
+            int led_color_0 = led_color[0];
+            int led_color_1 = led_color[1];
+            int led_color_2 = led_color[2];
+            int led_color_3 = led_color[3];
+
+            WS2812_Set_Color_1(led_color_0, led_color_1, led_color_2, led_color_3);
+        }
+        else if (6 == cmd)
+        {
+            JsonArray led_color_2 = doc["data"];
+            int led_color_2_0 = led_color_2[0];
+            int led_color_2_1 = led_color_2[1];
+            int led_color_2_2 = led_color_2[2];
+            int led_color_2_3 = led_color_2[3];
+
+            WS2812_Set_Color_2(led_color_2_0, led_color_2_1, led_color_2_2, led_color_2_3);
+        }
+        else if (7 == cmd)
+        {
+            bool alarm = doc["data"] == 1;
+            Buzzer_Alarm(alarm);
+        }
+        else if (8 == cmd)
+        {
+            JsonArray buzzer_data = doc["data"];
+            int alarm_on = buzzer_data[0] == 1;
+            int frequency_hz = buzzer_data[1];
+            Buzzer_Variable(alarm_on, frequency_hz);
+        }
+        else if (9 == cmd)
+        {
+            bool video_activation = doc["data"] == 1;
+            videoFlag = video_activation;
+        }
+
+        notifyClients();
+    }
+}
+
+void onEvent(AsyncWebSocket *server, AsyncWebSocketClient *client, AwsEventType type, void *arg, uint8_t *data, size_t len)
+{
+    switch (type)
+    {
+    case WS_EVT_CONNECT:
+        Serial.printf("WebSocket client #%u connected from %s\n", client->id(), client->remoteIP().toString().c_str());
+        break;
+    case WS_EVT_DISCONNECT:
+        Serial.printf("WebSocket client #%u disconnected\n", client->id());
+        break;
+    case WS_EVT_DATA:
+        handleWebSocketMessage(arg, data, len);
+        break;
+    case WS_EVT_PONG:
+    case WS_EVT_ERROR:
+        break;
+    }
+}
+
+void initWebSocket()
+{
+    ws.onEvent(onEvent);
+    server.addHandler(&ws);
+}
+
+void reconnect()
+{
+    // Loop until we're reconnected
+    while (!client.connected())
+    {
+        Serial.print("Attempting MQTT connection...");
+        // Attempt to connect
+        if (client.connect("ESP8266Client"))
+        {
+            Serial.println("connected");
+            // Subscribe
+            client.subscribe("esp32/output");
+        }
+        else
+        {
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            // Wait 5 seconds before retrying
+            delay(5000);
+        }
+    }
+}
+
+// void loopTask_Camera(void *pvParameters)
+// {
+//     while (1)
+//     {
+//         WiFiClient wf_client = server_Camera.available(); // listen for incoming clients
+//         if (wf_client)
+//         { // if you get a client
+//             Serial.println("Camera_Server connected to a client.");
+//             if (wf_client.connected())
+//             {
+//                 camera_fb_t *fb = NULL;
+//                 while (wf_client.connected())
+//                 { // loop while the client's connected
+//                     if (videoFlag == 0)
+//                     {
+//                         fb = esp_camera_fb_get();
+//                         if (fb != NULL)
+//                         {
+//                             uint8_t slen[4];
+//                             slen[0] = fb->len >> 0;
+//                             slen[1] = fb->len >> 8;
+//                             slen[2] = fb->len >> 16;
+//                             slen[3] = fb->len >> 24;
+//                             wf_client.write(slen, 4);
+//                             wf_client.write(fb->buf, fb->len);
+//                             Serial.println("Camera send");
+//                             esp_camera_fb_return(fb);
+//                         }
+//                     }
+//                 }
+//                 // close the connection:
+//                 wf_client.stop();
+//                 Serial.println("Camera Client Disconnected.");
+//                 ESP.restart();
+//             }
+//         }
+//     }
+// }
+
+void loopTask_Camera(void *pvParameters)
+{
+    while (1)
+    {
+        char size_buf[12];
+        WiFiClient wf_client = server_Camera.available(); // listen for incoming clients
+        if (wf_client)
+        { // if you get a client
+            Serial.println("Camera_Server connected to a client.");
+            if (wf_client.connected())
+            {
+                camera_fb_t *fb = NULL;
+                wf_client.write("HTTP/1.1 200 OK\r\nAccess-Control-Allow-Origin: *\r\nContent-Type: multipart/x-mixed-replace; boundary=" STREAM_CONTENT_BOUNDARY "\r\n");
+                while (wf_client.connected())
+                { // loop while the client's connected
+                    if (videoFlag == 1)
+                    {
+                        fb = esp_camera_fb_get();
+                        if (fb != NULL)
+                        {
+                            wf_client.write("\r\n--" STREAM_CONTENT_BOUNDARY "\r\n");
+                            wf_client.write("Content-Type: image/jpeg\r\nContent-Length: ");
+                            sprintf(size_buf, "%d\r\n\r\n", fb->len);
+                            wf_client.write(size_buf);
+                            wf_client.write(fb->buf, fb->len);
+
+                            uint8_t slen[4];
+                            slen[0] = fb->len >> 0;
+                            slen[1] = fb->len >> 8;
+                            slen[2] = fb->len >> 16;
+                            slen[3] = fb->len >> 24;
+                            wf_client.write(slen, 4);
+                            wf_client.write(fb->buf, fb->len);
+                            Serial.println("Camera send");
+                            esp_camera_fb_return(fb);
+                        }
+                    }
+                    else
+                    {
+                        // If videoFlag is 0, stop the client and exit the loop
+                        Serial.println("Stopping video stream...");
+                        wf_client.stop();
+                        Serial.println("Camera Client Disconnected.");
+                        ESP.restart();
+                    }
+                }
+                // close the connection:
+                wf_client.stop();
+                Serial.println("Camera Client Disconnected.");
+                // ESP.restart();
+            }
+        }
+    }
+}
